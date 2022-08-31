bc4a82a8fda452b1af2d1aa0ab58a3b682f47e34
Daniel <da.jaramim@gmail.com>
2022-08-31 14:32:08 -0400

    page login y recuperacion agregados

--- tellevoapp/src/app/app-routing.module.ts
+++ tellevoapp/src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'recuperacion',
+    loadChildren: () => import('./recuperacion/recuperacion.module').then( m => m.RecuperacionPageModule)
+  },
 ];
 
 @NgModule({
