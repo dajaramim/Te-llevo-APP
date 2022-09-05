b5ba9e0020b46fc12a010775fd280eedae13e516
Daniel Jaramillo <da.jaramim@gmail.com>
2022-09-05 15:29:55 -0300

    Creación del objeto user

--- tellevoapp/src/app/login/login.page.ts
+++ tellevoapp/src/app/login/login.page.ts
@@ -8,21 +8,23 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  public user: string = '';
-  public password: string = '';
+  public user = {
+    name: "",
+    password: ""
+  }
   constructor(private router: Router, private toastController: ToastController) { }
   
   async onLogin(position: 'top' | 'middle' | 'bottom') {
     const toast_error = await this.toastController.create({
-      message: 'Usuario o contraseña vacíos o incorrectos',
-      duration: 1500,
+      message: 'Usuario o contraseña incorrectos',
+      duration: 2500,
       position: position
     });
 
     let navigationExtras: NavigationExtras = {
-      state: {user: this.user}
+      state: {user: this.user.name}
     };
-    if (this.user.length === 0 || this.user.length < 6 || this.password.length === 0 || this.password.length < 8)
+    if (this.user.name.length === 0 || this.user.name.length < 6 || this.user.password.length === 0 || this.user.password.length < 8)
     {
       await toast_error.present();
     }
