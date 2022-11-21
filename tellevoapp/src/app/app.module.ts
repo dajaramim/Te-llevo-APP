d16780e9ad1386c9a4e9c2cb05bf75be0c04c02e
igtorrealba <81390090+igtorrealba@users.noreply.github.com>
2022-11-21 15:09:14 -0300

    ionic config

--- tellevoapp/src/app/app.module.ts
+++ tellevoapp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { isPlataform, IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,10 +10,51 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { HttpClientModule } from '@angular/common/http';
 
+// https://ionicframework.com/docs/angular/config
+const getConfig = () => {
+
+  let plataforma= '' ;
+
+  let config = {
+    animated:true,
+    hardwareBackButton:true,
+    swipeBackEnabled:true,
+    rippleEffect: true,
+    plataforma
+  };
+
+  if (isPlatform('iphone')){
+    config = {
+      ...config,
+      rippleEffect: false,
+
+    }
+    console.log(plataforma);
+  }
+  if (isPlatform('android')){
+    config = {
+      ...config,
+      rippleEffect: true,
+
+    }
+    console.log(plataforma);
+
+  }
+  if (isPlatform('mobileweb')){
+    config = {
+      ...config,
+      rippleEffect: false,
+
+
+    }
+    console.log(plataforma);
+  }
+  return config;
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot(), BrowserAnimationsModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(getConfig()), AppRoutingModule,IonicStorageModule.forRoot(), BrowserAnimationsModule,HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
